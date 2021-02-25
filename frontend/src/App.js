d25a58dd2ffb0c019dae9100b345db3f33d44221
N11itish <nitish.singh8050@gmail.com>
2021-02-25 14:34:43 +0530

    header and footer added with navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import { Container}  from 'react-bootstrap'
+import Footer from './components/Footer'
+import React from 'react';
+
+const App=()=> {
+  return (
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+     <h1>welcome</h1>
+     </Container>
+    </main>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
