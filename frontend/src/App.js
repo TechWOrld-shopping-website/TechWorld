92c6a589a940c7e0d3e9b724a02e881190994775
N11itish <nitish.singh8050@gmail.com>
2021-02-26 12:21:33 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-
+import Header from './components/Header'
+import { Container}  from 'react-bootstrap'
+import Footer from './components/Footer'
 import React from 'react';
 
 const App=()=> {
   return (
     <>
+    <Header/>
+    <main className="py-3">
+      <Container>
      <h1>welcome</h1>
+     </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
