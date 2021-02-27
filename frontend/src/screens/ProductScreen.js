f9c9377390ff8a340d933bed0af6d49f6b7da39c
sanjayprasadF5 <sanjayprasad2321998@gmail.com>
2021-02-27 17:34:02 +0530

    frontend and express bakend rounting

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,11 +3,67 @@ import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
+import Product from '../components/Product'
 const ProductScreen = ({ match }) => {
-    const product = products.find((p) => p._id === match.params.id)
- //   console.log(p._id)
-    console.log(product)
-    return <div>Product</div>
+
+    const product = products.find((p) => p._id == match.params.id)
+
+    return <>
+        <Link calssName='btn btn-light my-3' to='/'>Go Back</Link>
+        <Row>
+            <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={3}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating
+                            value={product.rating}
+                            text={`$(product.numReviews) reviews`} />
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                         Price: Rs{product.price}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                         Description: {product.description}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variants='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Price:</Col>
+                                <Col>
+                                    <strong>Rs. {product.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>Status:</Col>
+                                <Col>
+                                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button className='btn-block' type='button'>Add to Cart</Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                        
+                </Card>
+            </Col>
+        </Row>
+    
+    
+    
+    </>
+  
 }
 
 export default ProductScreen
