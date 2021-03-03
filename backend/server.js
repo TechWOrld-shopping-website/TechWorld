1db5adc74ea2cb54efe9d8bfaa9f1cc1fad74781
N11itish <nitish.singh8050@gmail.com>
2021-03-03 22:59:31 +0530

    User product order  js file added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js  '
-
+import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
 dotenv.config()
+
+connectDB()
 
 const app=express()
 
@@ -30,4 +33,4 @@ app.get('/api/products/:id',(req,res)=>
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`Server runing in ${process.env.NODE_ENV } mode on port ${PORT}`))
+app.listen(PORT,console.log(`Server runing in ${process.env.NODE_ENV } mode on port ${PORT}`.yellow.bold))
