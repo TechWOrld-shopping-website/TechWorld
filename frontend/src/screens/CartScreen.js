03eb28b493bbe7142ba37022efdf44f5df347f6e
DikshaTiwari-17 <diksha.lkt@gmail.com>
2021-03-09 17:27:54 +0530

    Cart Screen added, remove items done

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
     const productId = match.params.id
@@ -22,14 +22,18 @@ const CartScreen = ({ match, location, history }) => {
     }, [dispatch, productId,qty])
 
     const removeFromCartHandler = (id) => {
-        console.log('remove')
+        dispatch(removeFromCart(id))
+    }
+
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
     }
     
     return( 
         <Row>
             <Col md={8}>
                 <h1>Shopping Cart</h1>
-                {cartItems.length == 0 ? (
+                {cartItems.length === 0 ? (
                     <Message>
                         Your Cart is empty <Link to='/'>Go Back</Link>
                     </Message>
@@ -45,11 +49,11 @@ const CartScreen = ({ match, location, history }) => {
                                         <Link to={`/product/${item.product}`}>{item.name}
                                         </Link>
                                     </Col>
-                                    <Col md={2}>${item.price}</Col>
+                                    <Col md={2}>Rs.{item.price}</Col>
                                     <Col md={2}>
                                     <Form.Control 
                                             as='select' 
-                                            value={qty} 
+                                            value={item.qty} 
                                             onChange={(e) => dispatch(addToCart(item.product,
                                             Number(e.target.value)))}>
                                             {[...Array(item.countInStock).keys()].map((x) => (
@@ -72,7 +76,29 @@ const CartScreen = ({ match, location, history }) => {
                 )}
                 
             </Col>
-            <Col md={2}></Col>
+            <Col md={4}>
+                <Card>
+                    <ListGroup variant='flush'>
+                      <ListGroup.Item>
+                          <h2>
+                              Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)
+                              }) items
+                          </h2>
+                          Rs.{cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)
+                          .toFixed(2)}
+                          </ListGroup.Item>  
+                          <ListGroup.Item>
+                              <Button type='button' className='btn-block' disabled={cartItems.
+                              length === 0} onClick={checkoutHandler}>
+                                  Proceed To Checkout
+                              </Button>
+                          </ListGroup.Item>
+
+                    </ListGroup>
+
+                </Card>
+
+            </Col>
             
         </Row>
     
