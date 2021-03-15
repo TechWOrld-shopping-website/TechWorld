3cddbfa581fb55e3eeed61550ad339b00bb2d14c
quelingeeson <quelingeeson@gmail.com>
2021-03-15 14:50:41 +0530

    Shipping scrren,payment screen and create order screen -frontend and backend completed

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,8 @@ import {productListReducer, productDetailsReducer} from './reducers/productReduc
 
 import { cartReducer } from './reducers/cartReducers'
 import {userLoginReducer ,userRegisterReducer, userDetailsReducer,userUpdateProfileReducer} from './reducers/userReducers'
+import {orderCreateReducer} from './reducers/orderReducers'
+
 
 const reducer=combineReducers({
     productList:productListReducer, 
@@ -14,6 +16,7 @@ const reducer=combineReducers({
      userRegister : userRegisterReducer,
      userDetails : userDetailsReducer,
      userUpdateProfile : userUpdateProfileReducer,
+     orderCreate:orderCreateReducer,
 
 })
 
@@ -23,8 +26,11 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
 const userInfoFromStorage = localStorage.getItem('userInfo')
  ? JSON.parse(localStorage.getItem('userInfo')) : null
 
+ const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+ ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState={
-    cart: { cartItems: cartItemsFromStorage },
+    cart: { cartItems: cartItemsFromStorage ,shippingAddress: shippingAddressFromStorage },
     userLogin: {userInfo: userInfoFromStorage},
 }
 
