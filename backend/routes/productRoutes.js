b238708c4423cbeb705adab1c84e4adf7ff9e9fb
DikshaTiwari-17 <diksha.lkt@gmail.com>
2021-03-23 22:16:50 +0530

    Review and rating option added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,14 @@ import { getProducts,
      getProductById,
      deleteProduct,
      createProduct,
-     updateProduct
+     updateProduct,
+     createProductReview
      } from '../controllers/productControllers.js'
      import { protect, admin } from '../middleware/authMiddleware.js'
 const router=express.Router()
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 router.route('/:id').get(getProductById)
 .delete(protect,admin,deleteProduct).
 put(protect, admin, updateProduct)
